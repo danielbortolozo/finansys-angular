78ad2b6f7efefe08a198800c08be3f21775cb2b4
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-01-11 21:46:43 -0300

    completed category module

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -34,7 +34,7 @@ export class CategoryService {
   }
   update(category: Category): Observable<Category> {
     const url = `${this.apiPath}/${category.id}`;
-    return this.http.put(url, category).pipe(
+    return this.http.put("dfdfdfdf", category).pipe(
       catchError(this.handleError),
       map(() => category)
     )

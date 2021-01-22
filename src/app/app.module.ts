4ea8106e11370d835ca7db3a963602144cd81145
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-01-21 22:17:31 -0300

    Restructing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDatabase } from './in-memory-database';
-import { HttpClientModule } from '@angular/common/http';
-import { CategoriesModule } from './pages/categories/categories.module';
-import { CategoriesRoutingModule } from './pages/categories/categories-routing.module';
-import { EntriesModule } from './pages/entries/entries.module';
-import { EntriesRoutingModule } from './pages/entries/entries-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -20,18 +12,8 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
-    CategoriesModule,
-    CategoriesRoutingModule,
-    EntriesModule,
-    EntriesRoutingModule,
-    BrowserAnimationsModule,
-    BsDatepickerModule.forRoot()   
-
-    
+    CoreModule,    
+    AppRoutingModule   
   ],
   providers: [
     
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { CalendarModule } from 'primeng/calendar';
 import { IMaskModule } from 'angular-imask';
 // Datepicker module
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DatepickerModule } from 'ngx-bootstrap/datepicker';
+
+import { SharedModule }  from "../../shared/shared.module";
 
 @NgModule({
+  imports: [
+    SharedModule,
+    EntriesRoutingModule,    
+    CalendarModule,
+    IMaskModule    
+    
+         
+  ],
   declarations: [
     EntryListComponent,
     EntryFormComponent
-  ],
-  imports: [
-    CommonModule,
-    EntriesRoutingModule,
-    ReactiveFormsModule,
-    CalendarModule,
-    IMaskModule,    
-    DatepickerModule
-         
   ]
+ 
 })
 export class EntriesModule { }

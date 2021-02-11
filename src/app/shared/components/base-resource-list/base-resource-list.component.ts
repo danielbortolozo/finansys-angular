66c4215796bc3cd93c7f01dfae90637ffd34ba91
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-02-11 11:48:08 -0300

    improving categoryList with BaseResourceList

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -14,7 +14,7 @@ export abstract class BaseResourceListComponent<T extends BaseResourceModel> imp
 
   ngOnInit(): void {
     this.resourceService.getAll().subscribe(
-      resources => this.resource = resources.sort((a,b) => b.id - a.id),
+      resources => this.resources = resources.sort((a,b) => b.id - a.id),
       error => alert('Erro ao carregar a lista')
     )
   }
